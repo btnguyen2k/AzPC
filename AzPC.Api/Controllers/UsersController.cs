ec635fa342b1cf4d4dde0a6ae0a75765842170a5
Thanh Nguyen <btnguyen2k@gmail.com>
2025-03-11 00:37:02 +1100

    Restore API /api/users/-me

---
+++ AzPC.Api/Controllers/UsersController.cs
@@ -0,0 +1,54 @@
+﻿using AzPC.Api.Services;
+using AzPC.Shared.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace AzPC.Api.Controllers;
+
+public partial class UsersController : ApiBaseController
+{
+	private readonly IAuthenticator? Authenticator;
+	private readonly IAuthenticatorAsync? AuthenticatorAsync;
+	private readonly IIdentityRepository IdentityRepository;
+	private readonly IdentityOptions IdentityOptions;
+
+	public UsersController(
+		IIdentityRepository identityRepository,
+		IOptions<IdentityOptions> identityOptions,
+		IAuthenticator? authenticator,
+		IAuthenticatorAsync? authenticatorAsync)
+	{
+		ArgumentNullException.ThrowIfNull(identityRepository, nameof(identityRepository));
+		ArgumentNullException.ThrowIfNull(identityOptions, nameof(identityOptions));
+		if (authenticator == null && authenticatorAsync == null)
+		{
+			throw new ArgumentNullException("No authenticator defined defined.");
+		}
+
+		IdentityRepository = identityRepository;
+		IdentityOptions = identityOptions.Value;
+		Authenticator = authenticator;
+		AuthenticatorAsync = authenticatorAsync;
+	}
+
+	private async Task<(ActionResult?, AzPCUser)> VerifyAuthTokenAndCurrentUser()
+	{
+		var jwtToken = GetAuthToken();
+		var tokenValidationResult = await ValidateAuthTokenAsync(Authenticator, AuthenticatorAsync, jwtToken);
+		if (tokenValidationResult.Status != 200)
+		{
+			// the auth token should still be valid
+			return (ResponseNoData(403, tokenValidationResult.Error), null!);
+		}
+
+		var currentUser = await GetCurrentUserAsync(IdentityOptions, IdentityRepository);
+		if (currentUser == null)
+		{
+			// should not happen
+			return (_respAuthenticationRequired, null!);
+		}
+
+		return (null, currentUser);
+	}
+}
